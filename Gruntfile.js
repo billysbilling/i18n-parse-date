0f98d7df5982d333d7682e91cee942897d363f42
Sebastian Seilund <sebastian@seilund.com>
2014-02-18 13:53:25 -0800

    added initial files

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        'billy-builder': {
+            title: 'i18n-parse-date',
+            jshint: true
+        }
+    });
+
+    grunt.loadNpmTasks('billy-builder');
+};
